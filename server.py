68c7e58047c871eee452a9226af06e5f53c40fde
Tyga <tyga_ii@Tygas-MBP-2.ed.shawcable.net>
2023-09-27 23:05:48 -0600

    Mostly functional HTTP 1.1 compliant web server.     - Still gives 3 errors when running the runner.sh script     The tests that give errors (not failures) are as follows:         - test_deep_no_end         - test_get_deep         - test_hardcore

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 #  coding: utf-8 
+from pathlib import Path
 import socketserver
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
@@ -32,7 +33,55 @@ class MyWebServer(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        decoded = self.data.decode('utf-8')
+        arrivalHeaders = decoded.split("\n")
+        intermediary = arrivalHeaders[0].strip().split()
+        methodRequested = intermediary[0]
+        path = intermediary[1]
+        # print(methodRequested)
+        # print(path)
+
+        if not methodRequested == "GET":
+            self.request.sendall(bytearray("HTTP/1.1 405 Method not allowed\r\n\r\n",'utf-8'))
+
+        if not path.endswith("/"):
+            path += "/"
+        
+        # Set readPath to the current ./www plus the requested path
+        readPath = "./www" + path
+
+        # If file does not exist respond with 404 Not Found
+        if not (Path(readPath).exists()):
+            self.request.sendall(bytearray("HTTP/1.1 404 Not Found\r\npage not found\r\n",'utf-8'))
+
+        # Re-direct if no path is given
+        if path == "/":
+            response = "HTTP/1.1 301 Moved Permanently\r\nLocation: /index.html\r\n\r\n"
+            self.request.sendall(bytearray(response,'utf-8'))
+
+            content = self.readFile("./www/index.html")
+            departureHeaders = "HTTP/1.1 200 OK\r\nContent-Length: " + str(len(content)) + "\r\nContent-Type: text/html\r\nConnection: close\r\n\r\n"
+            response = departureHeaders + content + "\r\n"
+            self.request.sendall(bytearray(response,'utf-8'))
+        else:
+            content = self.readFile(readPath[:-1])
+            departureHeaders = "HTTP/1.1 200 OK\r\nContent-Length: " + str(len(content)) + "\r\n"
+            if readPath.endswith(".html/"):
+                departureHeaders += "Content-Type: text/html\r\n"
+            elif readPath.endswith(".css/"):
+                departureHeaders += "Content-Type: text/css\r\n"
+            else:
+                self.request.sendall(bytearray("HTTP/1.1 404 Not Found\r\npage not found\r\n",'utf-8'))
+            departureHeaders += "Connection: close\r\n\r\n"
+            response = departureHeaders + content + "\r\n"
+            self.request.sendall(bytearray(response,'utf-8'))
+
+    def readFile(self, filename):
+        file = open(filename, "r")
+        content = file.read()
+        file.close()
+        return content
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
